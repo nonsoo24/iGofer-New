efe82845d3b423ce6cdb320207d6f70db2444eb6
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-08-29 17:24:57 +0100

    Added Header to Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Shared/Header.vue';
 export default {
+  
   name: 'app',
   components: {
-    HelloWorld
+    'app-header': Header
   }
 }
 </script>
@@ -22,7 +21,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>

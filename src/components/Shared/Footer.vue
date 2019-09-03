84499dc7071dad29e98454c1c3be46f6efcc18db
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-09-03 18:09:36 +0100

    footer contents added

--- src/components/Shared/Footer.vue
+++ src/components/Shared/Footer.vue
@@ -4,16 +4,32 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-3">
-                        Company
+                        <h5>Company</h5>
+                        <ul class="list-unstyled">
+                            <li><a href="about.html">About Us</a></li>
+                            <li><a href="/contact-us">Contact Us</a></li>
+                            <li><a href="/blog">Blog</a></li>
+                            <li><a href="/careers">Careers</a></li>
+                            <li><a href="/privacy-policy">Privacy Policy</a></li>
+                            <li><a href="/terms-condition">Terms and Conditions</a></li>
+                        </ul>
                     </div>
                     <div class="col-md-3">
-                        Follow us
+                        <h5><strong>Follow us</strong></h5> <br>
+                        <a href="https://www.facebook.com/igoferNG" target="_blank"><i
+                                class="fab fa-facebook fa-3x"></i></a>
+                        <a href="https://twitter.com/igoferNG" target="_blank"><i class="fab fa-twitter fa-3x"></i></a>
+                        <a href="https://www.instagram.com/igoferng/" target="_blank"><i
+                                class="fab fa-instagram fa-3x"></i></a>
+                        <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
                     </div>
                     <div class="col-md-3">
-                        Location
+                        <h5><strong>Location</strong></h5> <br>
+                        <p>Norman Williams Street<br>off Awolowo Road, Ikoyi.</p>
                     </div>
                     <div class="col-md-3">
-                        Support
+                        <h5><strong>Support</strong></h5><br>
+                        <p>+234-8168847243 <br> hello@igofer.com.ng </p>
                     </div>
                 </div>
             </div>

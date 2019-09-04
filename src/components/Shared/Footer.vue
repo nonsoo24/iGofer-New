e2e3e48768a388110c1b476dc809b8a817396d33
Daniel Chukwurah <dnchukwurah@sidmach.com>
2019-09-04 15:47:26 +0100

    updated styling for homecontent component

--- src/components/Shared/Footer.vue
+++ src/components/Shared/Footer.vue
@@ -1,38 +1,38 @@
 <template>
     <div>
         <footer>
-            <div class="container-fluid">
-                <div class="row">
+                <div class="row container">
                     <div class="col-md-3">
-                        <h5>Company</h5>
+                        <h5>Information</h5>
                         <ul class="list-unstyled">
                             <li><a href="about.html">About Us</a></li>
-                            <li><a href="/contact-us">Contact Us</a></li>
                             <li><a href="/blog">Blog</a></li>
                             <li><a href="/careers">Careers</a></li>
+                            <li><a href="/faq">FAQs</a></li>
                             <li><a href="/privacy-policy">Privacy Policy</a></li>
                             <li><a href="/terms-condition">Terms and Conditions</a></li>
                         </ul>
                     </div>
+
                     <div class="col-md-3">
-                        <h5>Follow us</h5> <br>
-                        <a href="https://www.facebook.com/igoferNG" target="_blank"><i
-                                class="fab fa-facebook fa-3x"></i></a>
-                        <a href="https://twitter.com/igoferNG" target="_blank"><i class="fab fa-twitter fa-3x"></i></a>
-                        <a href="https://www.instagram.com/igoferng/" target="_blank"><i
-                                class="fab fa-instagram fa-3x"></i></a>
-                        <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin fa-3x"></i></a>
-                    </div>
-                    <div class="col-md-3">
-                        <h5>Location</h5> <br>
+                        <h5>Contact Us</h5>
                         <p>Norman Williams Street<br>off Awolowo Road, Ikoyi.</p>
-                    </div>
-                    <div class="col-md-3">
-                        <h5>Support</h5><br>
                         <p>+234-8168847243 <br> hello@igofer.com.ng </p>
                     </div>
+
+                      <div class="col-md-3">
+                        <h5>Follow us</h5>
+                        <a href="https://www.facebook.com/igoferNG" target="_blank"><i class="fab fa-facebook fa-2x text-light p-1"></i></a>
+                        <a href="https://twitter.com/igoferNG" target="_blank"><i class="fab fa-twitter fa-2x text-light p-1"></i></a>
+                        <a href="https://www.instagram.com/igoferng/" target="_blank"><i
+                                class="fab fa-instagram fa-2x text-light p-1"></i></a>
+                        <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin fa-2x text-light p-1"></i></a>
+                    </div>
+
+                    <div class="col-md-3">
+                        <h5>Support</h5><br>
+                    </div>
                 </div>
-            </div>
         </footer>
     </div>
 </template>
@@ -46,9 +46,33 @@ h5{
 font-family: Poppins;
 font-style: normal;
 font-weight: 600;
-font-size: 20px;
+font-size: 18px;
 line-height: 30px;
 color: #FFFFFF;
 margin-top: 4em;
 }
+
+li a{
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 27px;
+margin: 0px;
+padding:  0px;
+/* text-align: left !important; */
+color: #FFFFFF;
+}
+
+p{
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 27px;
+text-align: center;
+color: #FFFFFF;
+}
+
+
 </style>
